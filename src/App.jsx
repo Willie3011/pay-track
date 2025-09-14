a965a7f2cf98aa8ea8de6a485e5cd49e29e3297d
Willem April <willemapril33@gmail.com>
2025-09-14 19:17:34 +0200

    Add React Query and wrap app with QueryClientProvider
    Installed @tanstack/react-query and updated App.jsx to wrap the application in QueryClientProvider. This enables React Query for data fetching and caching throughout the app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { Routes, Route } from "react-router";
 import Homepage from "./pages/Homepage";
 import HomeLayout from "./Layouts/HomeLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <Routes>
-      <Route element={<HomeLayout/>}>
-        <Route index path="/" element={<Homepage />} />
-        <Route path="/about" element={<Homepage />} />
-        <Route path="/contact" element={<Homepage />} />
-        <Route path="/how-it-works" element={<Homepage />} />
-      </Route>
-    </Routes>
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route element={<HomeLayout />}>
+          <Route index path="/" element={<Homepage />} />
+          <Route path="/about" element={<Homepage />} />
+          <Route path="/contact" element={<Homepage />} />
+          <Route path="/how-it-works" element={<Homepage />} />
+        </Route>
+      </Routes>
+    </QueryClientProvider>
   );
 }
