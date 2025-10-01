c8d747bec09f4564b08a318ed119ed3fe9e8f7f0
Buddynice34 <willemapril33@gmail.com>
2025-10-01 16:21:05 +0200

    added functionality for back to top button

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -5,11 +5,17 @@ import { useState } from "react";
 
 const HomeLayout = () => {
     const [open, setOpen] = useState(false);
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        })
+    }
     return <main className="font-Poppins w-full min-h-screen relative scroll-smooth">
         <Navbar open={open} setOpen={setOpen} />
         <div onClick={() => setOpen(false)} className={`fixed inset-0 w-screen h-screen bg-black/50 z-40 ${open ? "block" : "hidden"}`}></div>
         <Outlet/>
-        <Footer/>
+        <Footer scroll={ scrollToTop} />
     </main>
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FaLinkedin } from "react-icons/fa6";
 import { IoIosArrowUp, IoLogoWhatsapp } from "react-icons/io";
 import { MdEmail, MdPhone } from "react-icons/md";
 
-const Footer = () => {
+const Footer = ({ scroll }) => {
     return (
         <footer className="bg-primary text-white w-full min-h-full">
             <div className="max-w-7xl mx-auto px-4 py-16">
@@ -12,7 +12,7 @@ const Footer = () => {
                         <h2 className="text-accent text-2xl font-bold mb-4">Pay Track</h2>
                         <p className="mb-4 max-w-[300px] text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum architecto similique natus dolore nihil libero vero aliquid quam soluta illum.</p>
                         <div className="socials">
-                            <ul className="flex gap-4 mb-6 text-lg">
+                            <ul className="flex gap-4 mb-6 text-2xl">
                                 <li>
                                     <a href="#" className="text-gray-200 hover:text-accent duration-300">
                                         <FaLinkedin />
@@ -36,7 +36,7 @@ const Footer = () => {
                             </ul>
                         </div>
 
-                        <button className="w-min whitespace-nowrap flex items-center gap-2 border border-accent rounded-lg text-accent hover:text-primary hover:bg-accent p-4 uppercase text-sm cursor-pointer duration-300"><IoIosArrowUp /> Back to top</button>
+                        <button onClick={scroll} className="w-min whitespace-nowrap flex items-center gap-2 border border-accent rounded-lg text-accent hover:text-primary hover:bg-accent p-4 uppercase text-sm cursor-pointer duration-300"><IoIosArrowUp /> Back to top</button>
                     </div>
                     <div className="w-full flex flex-col max-sm:justify-center max-sm:items-center">
                         <h3 className="text-lg fron-medium mb-4">Site Map</h3>
